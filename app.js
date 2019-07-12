482bdedf5cd67e757288a500953ef348f8a97fba
mag16 <marcoguzman16@gmail.com>
2019-07-11 20:12:21 -0700

    express added, port added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-test
+const express = require("express");
+
+const app = express();
+const bodyparser = require("body-parser");
+
+const port = process.env.PORT || 3200;
+
+//middleware used
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
+
+const orders = [];
